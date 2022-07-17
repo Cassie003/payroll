ecef2eb968d0238b705751a36733653a05e7c462
casey minu <96833433+Cassie003@users.noreply.github.com>
2022-07-17 19:41:07 +0300

    payroll

---
+++ main (3).c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+//Githongas Factory//
+//by. lewis kirimi//
+int main()
+{
+    int totalhoursWorked- 40
+    int krapin,emloymentNo, hoursWorked, normalPayment, overtime,netIncome;
+    char name{100};
+    
+printf ( "Welcome to Githongas factory\n");
+printf("enter your name\n");
+scanf ("%n,&name");
+
+printf("enter your employee number\n");
+scanf("%d, employmentNo");
+
+printf("enter  your KRA pin \n");
+scanf("%d ,krapin\n");
+
+printf("enter number of hours worked\n");
+scanf("%d, hoursWorked\n");
+
+  normalPay =1000 * totalhoursWorked;
+  overtimePay = (hourWorked - totalhoursWorked)* 1500;
+  taxPaid =normalpay * 30/100;
+  netIncome =(normalpay -taxPaid)+ overtimePay
+  
+printf("normalPay: %d\n, normalPay");
+printf("overtimePay: %d\n, overtimePay");
+printf("taxPaid: %d\n, taxPaid");
+printf("netIncome:%d\n, netIncome");
+
+    return 0;
+}
